0b4cf18d6613cd76c14aed1fb9718b368abff748
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-09-20 23:44:37 +0200

    Linked List

---
+++ src/main/java/DataStructures/LinkedList/LinkedList.java
@@ -0,0 +1,66 @@
+/**
+ * @author giuliano ranauro
+ * Date: 20/09/2021 23:17
+ * Ide: Intellij
+ * JDK: 1.8
+ * @version 1.0
+ */
+package DataStructures.LinkedList;
+
+public class LinkedList {
+    private Node head;      //the first node
+
+
+    public LinkedList(){}
+
+    public void insert(int valueToInsert){
+        Node newNode = new Node(valueToInsert);
+
+        if (this.head == null){
+            this.head = newNode;
+            return;
+        }
+
+        Node current = this.head;
+
+
+        while (current.getNext() != null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void printList(){
+        Node current = this.head;
+        while (current != null){
+            System.out.println(current.getValue());
+            current = current.getNext();
+        }
+    }
+
+
+    private class Node{
+        private int value;      // the node value
+        private Node next;      // the next node
+
+        Node(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+}
---
+++ src/main/java/DataStructures/LinkedList/Tester.java
@@ -0,0 +1,20 @@
+/**
+ * @author giuliano ranauro
+ * Date: 20/09/2021 23:23
+ * Ide: Intellij
+ * JDK: 1.8
+ * @version 1.0
+ */
+package DataStructures.LinkedList;
+
+public class Tester {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(3);
+        list.insert(2);
+        list.insert(4);
+
+        list.printList();
+    }
+}
